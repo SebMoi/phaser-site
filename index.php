5752af45496cac3e993f1bad6ae48e04acd895e1
Richard Davey <rdavey@gmail.com>
2013-09-13 11:22:12 +0100

    Launch web site

---
+++ index.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>phaser - HTML5 game framework</title>
+	<meta name="description" content="A free, fast and flexible framework for HTML5 game creation">
+	<link media="all" rel="stylesheet" href="css/all.css">
+	<link rel="author" href="https://plus.google.com/106676047029022880747/"/>
+	<meta property="og:title" content="phaser - HTML5 game framework"/>
+	<meta property="og:url" content="http://phaser.io"/>
+	<meta property="og:description" content="A free, fast and flexible framework for HTML5 game creation"/>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script >window.jQuery || document.write('<script src="js/jquery-1.8.3.min.js"><\/script>')</script>
+	<script src="js/jquery.main.js"></script>
+	<!--[if IE]><script src="js/ie.js"></script><![endif]-->
+	<script src="//use.typekit.net/fmc0gtt.js"></script>
+	<script >try{Typekit.load();}catch(e){}</script>
+</head>
+<body>
+	<?php include_once("ga.php") ?>
+	<div id="wrapper">
+		<div class="w1">
+			<header id="header">
+				<div class="logo-holder">
+					<strong class="logo"><a href="http://phaser.io">Phaser</a></strong>
+					<a href="http://phaser.io" class="logotype"><img src="images/logotype.png" width="82" height="114" alt="HTML5"></a>
+				</div>
+				<section class="container">
+					<h1 class="head">Desktop and Mobile HTML5 game framework</h1>
+					<div class="text">
+						<p>A fast, free and fun open source framework supporting both JavaScript and TypeScript</p>
+					</div>
+					<a href="https://github.com/photonstorm/phaser" class="btn">Download and get started Download or Fork via Github</a>
+				</section>
+			</header>
+			<div id="main">
+				<section class="section">
+					<h1><span class="featured">Phaser Features</span></h1>
+					<div class="about">
+						<ul>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Easy Asset Loading</a>
+									<div class="popup">
+										<h2>Easy Asset Loading</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Multiple Cameras</a>
+									<div class="popup">
+										<h2>Multiple Cameras</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Sprites</a>
+									<div class="popup">
+										<h2>Sprites</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Groups</a>
+									<div class="popup">
+										<h2>Groups</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Animation</a>
+									<div class="popup">
+										<h2>Animation</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Scroll Zones</a>
+									<div class="popup">
+										<h2>Scroll Zones</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Collision &amp; physics</a>
+									<div class="popup">
+										<h2>Collision &amp; physics</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Particles</a>
+									<div class="popup">
+										<h2>Particles</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+						</ul>
+						<div class="image-holder"><img src="images/img-02.png" width="488" height="275" alt="game screen shot"></div>
+						<ul class="additional">
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Input</a>
+									<div class="popup">
+										<h2>Input</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">GAME UI</a>
+									<div class="popup">
+										<h2>GAME UI</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">World</a>
+									<div class="popup">
+										<h2>World</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Sound</a>
+									<div class="popup">
+										<h2>Sound</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">MOBILE BROWSER</a>
+									<div class="popup">
+										<h2>MOBILE BROWSER</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">GAME SCALING</a>
+									<div class="popup">
+										<h2>GAME SCALING</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Tilemaps</a>
+									<div class="popup">
+										<h2>Tilemaps</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="popup-holder">
+									<a class="open" href="#">Plugin system</a>
+									<div class="popup">
+										<h2>Plugin system</h2>
+										<p>The Stage is the place on which your game lives. You can easily change its position within the browser, width, height, background colour (have your game on a transparent background) and StageScaleMode.</p>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</section>
+				<section class="section add">
+					<h1>Getting Started</h1>
+					<div class="box-holder">
+						<div class="box one">
+							<h2><mark>1.</mark> Download</h2>
+							<p>Download or create a fork of the Phaser github repository:</p>
+							<a href="https://github.com/photonstorm/phaser" class="button item-01"><span>Download via Github</span></a>
+						</div>
+						<div class="box two">
+							<h2><mark>2.</mark> Setup</h2>
+							<p>Get started with these guides:</p>
+							<div class="button-frame">
+								<a href="#comingsoon" class="button item-02"><span>TypeScript Guide</span></a>
+								<a href="#comingsoon" class="button item-03"><span>JavaScript Guide</span></a>
+							</div>
+						</div>
+						<div class="box three">
+							<h2><mark>3.</mark> Create!</h2>
+							<p>Create your awesome game with Phaser then share it with everyone in the official forums!</p>
+							<a href="http://www.html5gamedevs.com/forum/14-phaser/" class="button item-04"><span>Join the Forums</span></a>
+						</div>
+					</div>
+				</section>
+				<div class="gallery-holder">
+					<h1>Phaser Made Games</h1>
+					<div class="carousel">
+						<div class="mask">
+							<div class="slideset">
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-04.jpg" width="310" height="200" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-03.jpg" width="310" height="200" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">the community game!</a></strong>Author Name</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-02.jpg" width="310" height="200" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">Peter Rabbit: Hide and Seek</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-04.jpg" width="310" height="205" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-03.jpg" width="310" height="205" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">the community game!</a></strong>Author Name</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-02.jpg" width="310" height="205" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">Peter Rabbit: Hide and Seek</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-04.jpg" width="310" height="205" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="#"><img src="images/img-03.jpg" width="310" height="205" alt="image description"></a>
+										<figcaption><strong class="title"><a href="#">the community game!</a></strong>Author Name</figcaption>
+									</figure>
+								</div>
+							</div>
+						</div>
+						<a class="btn-prev" href="#">Previous</a>
+						<a class="btn-next" href="#">Next</a>
+					</div>
+				</div>
+			</div>
+			<footer id="footer">
+				<div class="footer-holder">
+					<ul class="social-networks">
+						<li><a class="forum" href="http://www.html5gamedevs.com/forum/14-phaser/">Phaser Forums</a></li>
+						<li><a class="twitter" href="https://twitter.com/photonstorm">@photonstorm</a></li>
+						<li><a class="github" href="https://github.com/photonstorm/phaser">Phaser on Github</a></li>
+					</ul>
+					<div class="holder">
+						<div class="text-holder">
+							<strong class="logotype-01"><a href="http://www.photonstorm.com">Photon Storm</a></strong>
+							<strong class="copy">&copy; 2013 Photon Storm Ltd.</strong>
+							<span>All rights reserved.</span>
+						</div>
+						<div class="logo-holder">
+							<a href="http://flixel.org"><img src="images/logotype-02.png" width="26" height="50" alt="image description"></a>
+							<div class="description">
+								<span>Looking for a Flash game framework?</span>
+								<a class="link" href="http://flixel.org">Try Flixel</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
+	</div>
+</body>
+</html>
