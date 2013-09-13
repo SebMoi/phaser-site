125151b2ad8061c6f7d9eb35d22d7b84fe1918f5
Richard Davey <rdavey@gmail.com>
2013-09-13 13:33:37 +0100

    Updated screen shots

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>phaser - HTML5 game framework</title>
+	<title>Phaser - Desktop and Mobile HTML5 game framework</title>
 	<meta name="description" content="A free, fast and flexible framework for HTML5 game creation">
 	<link media="all" rel="stylesheet" href="css/all.css">
 	<link rel="author" href="https://plus.google.com/106676047029022880747/"/>
@@ -14,7 +14,7 @@
 	<script src="js/jquery.main.js"></script>
 	<!--[if IE]><script src="js/ie.js"></script><![endif]-->
 	<script src="//use.typekit.net/fmc0gtt.js"></script>
-	<script >try{Typekit.load();}catch(e){}</script>
+	<script>try{Typekit.load();}catch(e){}</script>
 </head>
 <body>
 	<?php include_once("ga.php") ?>
@@ -173,7 +173,7 @@
 									<a class="open" href="#">Developer Support</a>
 									<div class="popup">
 										<h2>Developer Support</h2>
-										<p>We use Phaser every day on our many client projects. As a result it's constantly evolving and improving and we jump on bugs and pull requests quickly. This is a living, breathing framework maintained by a commercial company with custom feature development and support packages available.</p>
+										<p>We use Phaser every day on our many client projects. As a result it's constantly evolving and improving and we jump on bugs and pull requests quickly. This is a living, breathing framework maintained by a commercial company with custom feature development and support packages available. We live and breathe HTML5 games.</p>
 									</div>
 								</div>
 							</li>
@@ -201,8 +201,8 @@
 							<h2><mark>2.</mark> Setup</h2>
 							<p>Get started with these guides:</p>
 							<div class="button-frame">
-								<a href="#comingsoon" class="button item-02"><span>TypeScript Guide</span></a>
 								<a href="#comingsoon" class="button item-03"><span>JavaScript Guide</span></a>
+								<a href="#comingsoon" class="button item-02"><span>TypeScript Guide</span></a>
 							</div>
 						</div>
 						<div class="box three">
@@ -219,50 +219,56 @@
 							<div class="slideset">
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-04.jpg" width="310" height="200" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
+										<a href="http://www.photonstorm.com/html5/clients/our-largest-html5-game-to-date-wolfblood-the-mystery-of-stoneybridge-for-cbbc"><img src="images/shot-wolfblood.jpg" width="310" height="200" alt="Wolfblood: The Mystery of Stoneybridge"></a>
+										<figcaption><strong class="title"><a href="http://www.photonstorm.com/html5/clients/our-largest-html5-game-to-date-wolfblood-the-mystery-of-stoneybridge-for-cbbc">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-03.jpg" width="310" height="200" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">the community game!</a></strong>Author Name</figcaption>
+										<a href="http://www.html5gamedevs.com/topic/1223-connect-me-factory-my-phaser-game/"><img src="images/shot-connect-me.jpg" width="310" height="200" alt="Connect Me Factory"></a>
+										<figcaption><strong class="title"><a href="http://www.html5gamedevs.com/topic/1223-connect-me-factory-my-phaser-game/">Connect Me Factory</a></strong>By Haden</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-02.jpg" width="310" height="200" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">Peter Rabbit: Hide and Seek</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
+										<a href="http://www.photonstorm.com/games/peter-rabbit-hide-and-seek"><img src="images/shot-peterrabbit.jpg" width="310" height="200" alt="Peter Rabbit: Hide and Seek"></a>
+										<figcaption><strong class="title"><a href="http://www.photonstorm.com/games/peter-rabbit-hide-and-seek">Peter Rabbit: Hide and Seek</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-04.jpg" width="310" height="205" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
+										<a href="http://www.photonstorm.com/games/sarah-and-duck-jigsaw"><img src="images/shot-sarahandduck.jpg" width="310" height="200" alt="Sarah and Duck: Jigsaw"></a>
+										<figcaption><strong class="title"><a href="http://www.photonstorm.com/games/sarah-and-duck-jigsaw">Sarah and Duck: Jigsaw</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-03.jpg" width="310" height="205" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">the community game!</a></strong>Author Name</figcaption>
+										<a href="http://www.ludumdare.com/compo/ludum-dare-27/?action=preview&amp;uid=25563"><img src="images/shot-okai.jpg" width="310" height="200" alt="OKAI"></a>
+										<figcaption><strong class="title"><a href="http://www.ludumdare.com/compo/ludum-dare-27/?action=preview&amp;uid=25563">OKAI</a></strong>By NoNameNoSocks</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-02.jpg" width="310" height="205" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">Peter Rabbit: Hide and Seek</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
+										<a href="http://www.gamepyong.com/til_death/"><img src="images/shot-tildeath.jpg" width="310" height="200" alt="Til Death"></a>
+										<figcaption><strong class="title"><a href="http://www.gamepyong.com/til_death/">Til Death</a></strong>By GamePyong</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-04.jpg" width="310" height="205" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">Wolfblood</a></strong>By Photon Storm. Client: BBC / CBBC</figcaption>
+										<a href="http://www.gamepyong.com/caveman_calamity/"><img src="images/shot-caveman.jpg" width="310" height="200" alt="Caveman Calamity"></a>
+										<figcaption><strong class="title"><a href="http://www.gamepyong.com/caveman_calamity/">Caveman Calamity</a></strong>By GamePyong</figcaption>
 									</figure>
 								</div>
 								<div class="slide">
 									<figure>
-										<a href="#"><img src="images/img-03.jpg" width="310" height="205" alt="image description"></a>
-										<figcaption><strong class="title"><a href="#">the community game!</a></strong>Author Name</figcaption>
+										<a href="http://www.photonstorm.com/games/old-jacks-boat"><img src="images/shot-oldjacksboat.jpg" width="310" height="200" alt="Old Jack's Boat"></a>
+										<figcaption><strong class="title"><a href="http://www.photonstorm.com/games/old-jacks-boat">Old Jack's Boat: Find the Fish</a></strong>By Photon Storm. Client: BBC / CBeebies</figcaption>
+									</figure>
+								</div>
+								<div class="slide">
+									<figure>
+										<a href="http://www.html5gamedevs.com/topic/873-guidelines-for-posting-in-this-board/"><img src="images/shot-yourgame.jpg" width="310" height="200" alt="Your Game Here!"></a>
+										<figcaption><strong class="title"><a href="http://www.html5gamedevs.com/topic/873-guidelines-for-posting-in-this-board/">Your Game Here!</a></strong>By the wonderful person reading this :)</figcaption>
 									</figure>
 								</div>
 							</div>
